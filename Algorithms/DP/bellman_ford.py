52bd647d01d3668f60ca0250dcc6456b8b732bdb
ravi.kumar <sitravi.kumar@gmail.com>
2016-08-31 12:18:29 +0530

    DP commit

---
+++ Algorithms/DP/bellman_ford.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+"""
+Problem : The Bellman ford algorithm for calculating single source shortest paths
+				- CLRS style
+"""
+
+graph = { 
+    's' : {'t':6, 'y':7},
+    't' : {'x':5, 'z':-4, 'y':8 },
+    'y' : {'z':9, 'x':-3},
+    'z' : {'x':7, 's': 2},
+    'x' : {'t':-2}
+}
+
+INF = float('inf')
+
+dist={}
+
+predecessor ={}
+
+def initialize_single_source(graph,s):
+	for v in graph:
+		dist[v]=INF
+		predecessor[v]=None
+	dist[s]=0
+
+def relax(graph,u,v):
+	if(dist[v]>dist[u]+graph[u][v]):
+		dist[v]=dist[u]+graph[u][v]
+		predecessor[v]=u
+
+def bellman_ford(graph,s):
+	initialize_single_source(graph,s)
+	edges=[(u,v) for u in graph for v in graph[u].keys()]
+	number_vertices=len(graph)
+	for i in range(number_vertices-1):
+		for (u,v) in edges:
+			relax(graph,u,v)
+	for (u,v) in edges:
+		if dist[v]>dist[u]+graph[u][v]:
+			return False
+	return True
+
+def get_distances(graph,s):
+	if bellman_ford(graph,s):
+		return dist
+	return "Graph Contains a negative cycle"
+
+if __name__=="__main__":
+	print get_distances(graph,'s')
+
